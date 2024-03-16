4249a1aadfe521d08be2dac81c82910aa3f4d6a9
iSitanshu <111778981+iSitanshu@users.noreply.github.com>
2024-03-16 07:54:19 +0000

    01variable

---
+++ chai_aur_js/1variables.js
@@ -0,0 +1,18 @@
+const accountID = 123428572
+let accountName = "Sitanshu"
+var accountPass = "12345"
+accountCity = "lucknow"
+
+// accountID = 2
+
+accountName = "Mishra"
+accountPass = "09876"
+accountCity = "Delhi NCR"
+
+console.log(accountID);
+
+console.log(accountName);
+console.log(accountPass);
+console.log(accountCity);
+
+console.table([accountID,accountName,accountPass,accountCity])
