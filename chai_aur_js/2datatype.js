d7852d9630a16ba09c2d950c9e0a59b7b3de2e4b
iSitanshu <111778981+iSitanshu@users.noreply.github.com>
2024-03-16 07:54:35 +0000

    02Datatype

---
+++ chai_aur_js/2datatype.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let name="Sitanshu" //string
+let number=171 //number 
+let check=true; //boolean
+let state; //undefined
+let state2=null //standalone value
+//symbol generally used in react - bohot saare component banate hai aur fir identify krna hota hai uniqueness 
+//FIGMA tool use for "unique"
+
+//object
+
+console.log(typeof "Sitanshu") //string
+console.log(typeof check) //boolean
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
