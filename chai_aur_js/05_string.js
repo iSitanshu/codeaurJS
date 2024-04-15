08883c3789e0a171bfe0006fcf719e23de1e6322
iSitanshu <111778981+iSitanshu@users.noreply.github.com>
2024-04-15 21:00:14 +0000

    string

---
+++ chai_aur_js/05_string.js
@@ -0,0 +1,26 @@
+const name = "Sitanshu"
+const repocount = 50
+// console.log(name+" have over "+repocount+" repository");
+// console.log(`Hello my name is ${name} and my repo count is ${repocount}`);
+
+const gamename = new String('Sitanshun')
+// console.log(gamename[0])
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+
+// const newstring = gamename.substring(0,4)
+// console.log(newstring)
+
+const anotherstring = gamename.slice(-8,4)
+console.log(anotherstring)
+const newstringone = "  sitanshu    "
+console.log(newstringone)
+console.log(newstringone.trim())
+
+
+const url = "https://hitesh.com/hitesh%20chaudhary"
+console.log(url.replace("%20"," "))
+
+console.log(gamename.split())
