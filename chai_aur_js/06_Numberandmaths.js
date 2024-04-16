3e90b2bcf003f0aee7e0fcb2de75cc148d458720
iSitanshu <111778981+iSitanshu@users.noreply.github.com>
2024-04-16 22:58:21 +0000

    Math and Num

---
+++ chai_aur_js/06_Numberandmaths.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+const othernum = 23.94534543
+// console.log(othernum.toPrecision(3))
+
+// +++++++++++ Maths ++++++++++++
+
+//  console.log(Math)
+//  console.log(Math.abs(-2))
+//  console.log(Math.round(4.6))
+//  console.log(Math.max(4,2,3,6,8))
+//  console.log(Math.min(4,2,3,6,8))
+
+// console.log(Math.random())
+// console.log(Math.random()*10+1) //randon such that minimum 1
+// console.log(Math.floor(Math.random()*10+1)) //randon such that minimum 1
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.randon() * (max-min+1)) + min)
