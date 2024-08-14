6b8592b71749c5f1c25d44b4990f6612cd0b10ac
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-14 16:58:57 +0530

    Check

--- chai_aur_js_03/04_IIFE.js
+++ chai_aur_js_03/04_IIFE.js
@@ -12,7 +12,6 @@
 // ()() - global scope ke pollution se problem hoti hai kai baar 
 // global scope/variable k ppollution o hatane k liye IIFE
 
-
 // named iife
 // (function chaiaurjs(){ 
 //     console.log("DB CONNECTED 2");
