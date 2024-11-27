1c83878230088f328812ef26d2ff616d04c0adbb
iSitanshu <Sitanshumishra18@gmail.com>
2024-11-27 14:26:17 +0530

    Added auto save extension

--- chai_aur_js_01(variables)/doubt.js
+++ chai_aur_js_01(variables)/doubt.js
@@ -2,3 +2,9 @@
 // console.log(2 == 1);
 // console.log(1 == "1");
 console.log(2);
+console.log("Sora");
+
+console.log("EMptyt")
+console.log("sfsdjfhsjdfhsdjfh")
+console.log("d")
+console.log("fklsdlfjs")
